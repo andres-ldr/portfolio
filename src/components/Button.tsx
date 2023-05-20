3259bfbf6c2c35cc7f4cb35353ec5e83748f9c76
Andrés <lopez.andresrigoberto@gmail.com>
2023-05-20 10:33:10 -0500

    [add] btn

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button: React.FC = () => {
+  return (
+    <div className='rounded-2xl shadow-md pt-4 pb-4 pl-9 pr-9 cursor-pointer font-bold transition-all hover:shadow-lg hover:bg-slate-900 hover:text-white'>
+      Contact me
+    </div>
+  );
+};
+
+export default Button;
