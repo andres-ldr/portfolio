969f6e608ef9b802f251fc965efd52e8b78a114e
Andrés <lopez.andresrigoberto@gmail.com>
2023-05-20 10:29:56 -0500

    [add] install packages and tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      inter: ['inter, sans-serif'],
+    },
+    extend: {
+      keyframes: {
+        show: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        show: 'show 0.75s ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+};
