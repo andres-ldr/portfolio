ee173a124130b2b81d10ccd3dc08d7645f522c30
Andrés <lopez.andresrigoberto@gmail.com>
2023-05-21 12:05:30 -0500

    [add] id

--- src/components/JobDetails.tsx
+++ src/components/JobDetails.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 interface JDProps {
   description: string;
   goals: string[];
-  skills: string[];
+  stack: string[];
   isSelected: boolean;
 }
 const JobDetails: React.FC<JDProps> = ({
   description,
   goals,
-  skills,
+  stack,
   isSelected,
 }) => {
   return (
@@ -20,7 +20,7 @@ const JobDetails: React.FC<JDProps> = ({
       <h4 className='text-2xl font-semibold text-left'>Description:</h4>
       <p className='text-left text-lg'>{description}</p>
       <h4 className='text-2xl font-semibold text-left'>Reached goals:</h4>
-      {/* skills */}
+      {/* stack */}
       <ul className='text-left text-lg list-disc space-y-5'>
         {goals.map((goal) => (
           <li key={goal} className=''>
@@ -30,7 +30,7 @@ const JobDetails: React.FC<JDProps> = ({
       </ul>
       {/* tags */}
       <div className='flex mt-12 flex-wrap'>
-        {skills.map((skill) => (
+        {stack.map((skill) => (
           <div
             key={skill}
             className='bg-red-300 rounded-full m-2 pt-2 pb-2 pl-4 pr-4 border-red-600 border-2 text-red-950 font-semibold'
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from './Button';
 
 const Navbar: React.FC = () => {
+  useEffect(() => {
+    console.log('hi mom');
+  }, []);
+
+  const executeScroll = (id: string) => {
+    console.log(id);
+    let element = document.getElementById(id) as HTMLElement;
+    element.scrollIntoView();
+  };
+
   return (
     <div className='fixed bg-slate-100 top-0 w-full flex justify-center z-20'>
       <div className='w-3/4 flex justify-between bg-transparent p-6 z-20'>
         <h1 className='text-3xl'>Logo</h1>
         <div className='flex space-x-14 leading-none'>
-          <a href='#' className='font-bold text-xl leading-none p-2'>
+          <a
+            href='#about-me'
+            className='font-bold text-xl leading-none p-2  hover-underline-animation'
+          >
             About
           </a>
-          <a href='#' className='font-bold text-xl leading-none p-2'>
+          <a
+            href='#'
+            className='font-bold text-xl leading-none p-2  hover-underline-animation'
+          >
             Skillset
           </a>
-          <a href='#' className='font-bold text-xl leading-none p-2'>
+          <a
+            href='#'
+            className='font-bold text-xl leading-none p-2  hover-underline-animation'
+          >
             Experience
           </a>
-          <a href='#' className='font-bold text-xl leading-none p-2'>
+          <a
+            href='#'
+            className='font-bold text-xl leading-none p-2  hover-underline-animation'
+          >
             Portfolio
           </a>
           <Button />
