ff0a6bec9cbbc089f96426520c72e0a5372d2ae4
Andrés <lopez.andresrigoberto@gmail.com>
2023-06-02 19:09:14 -0700

    [add] ItemGrid components with Intersection Observer

---
+++ src/components/grids/ItemGrid.tsx
@@ -0,0 +1,329 @@
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import CardProjContent from '../CardProjectContent';
+
+interface ItemGridProps {
+  type: number;
+  total_items: number;
+  data: any;
+  visibilityHandler: (id?: number) => void;
+}
+
+const ItemGrid: React.FC<ItemGridProps> = ({
+  type,
+  data,
+  total_items,
+  visibilityHandler,
+}) => {
+  const item = useRef<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const [n, setN] = useState(type);
+
+  const options = {
+    root: null,
+    rootMarging: '0px',
+    threshold: 0.3,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        const [entry] = entries;
+        if (entry.isIntersecting) {
+          setIsVisible(entry.isIntersecting);
+          observer.unobserve(entry.target);
+        }
+      },
+      options
+    );
+
+    if (item.current) observer.observe(item.current);
+
+    return () => {
+      if (item.current) observer.unobserve(item.current);
+    };
+  }, [item, options]);
+
+  const pickItemType = () => {
+    switch (total_items) {
+      case 5:
+        switch (n) {
+          case 0:
+            return (
+              <div
+                ref={item}
+                key={data.id}
+                style={{ backgroundImage: `url("${data.images[0]}")` }}
+                className={`${
+                  isVisible
+                    ? 'card-grid col-span-2 row-span-3 max-lg:col-span-1 max-lg:row-span-1'
+                    : 'opacity-0'
+                } `}
+              >
+                <CardProjContent
+                  id={data.id}
+                  name={data.name}
+                  stack={data.stack}
+                />
+              </div>
+            );
+          case 1:
+            return (
+              <div
+                ref={item}
+                key={data.id}
+                style={{ backgroundImage: `url("${data.images[0]}")` }}
+                className={`${
+                  isVisible
+                    ? 'card-grid col-span-2 row-span-2 max-lg:col-span-1 max-lg:row-span-1'
+                    : 'opacity-0'
+                } `}
+              >
+                <CardProjContent
+                  id={data.id}
+                  name={data.name}
+                  stack={data.stack}
+                />
+              </div>
+            );
+          case 2:
+            return (
+              <div
+                ref={item}
+                key={data.id}
+                style={{ backgroundImage: `url("${data.images[0]}")` }}
+                className={`${
+                  isVisible
+                    ? 'card-grid col-span-2 row-span-2 col-start-3 row-start-1 max-lg:col-span-1 max-lg:row-span-1'
+                    : 'opacity-0'
+                }  `}
+              >
+                <CardProjContent
+                  id={data.id}
+                  name={data.name}
+                  stack={data.stack}
+                />
+              </div>
+            );
+          case 3:
+            return (
+              <div
+                ref={item}
+                key={data.id}
+                style={{ backgroundImage: `url("${data.images[0]}")` }}
+                className={`${
+                  isVisible
+                    ? 'card-grid col-span-2 row-span-3 col-start-3 row-start-3 max-lg:col-span-1 max-lg:row-span-1'
+                    : 'opacity-0'
+                }  `}
+              >
+                <CardProjContent
+                  id={data.id}
+                  name={data.name}
+                  stack={data.stack}
+                />
+              </div>
+            );
+          default:
+            return (
+              <div
+                ref={item}
+                key={data.id}
+                style={{ backgroundImage: `url("${data.images[0]}")` }}
+                className={`${
+                  isVisible
+                    ? 'card-grid col-span-2 row-span-5 col-start-5 row-start-1 max-lg:col-span-1 max-lg:row-span-1'
+                    : 'opacity-0'
+                }  `}
+              >
+                <CardProjContent
+                  id={data.id}
+                  name={data.name}
+                  stack={data.stack}
+                />
+              </div>
+            );
+        }
+      case 6:
+        switch (n) {
+          case 0:
+            return (
+              <div
+                ref={item}
+                key={data}
+                className={
+                  isVisible ? `image-card col-span-4 row-span-3` : 'opacity-0'
+                }
+              >
+                {/* image */}
+                <div
+                  key={data}
+                  style={{
+                    backgroundImage: `url("${data}")`,
+                  }}
+                  className={`image-card__image`}
+                ></div>
+                {/* cover */}
+                <div className='image-card__cover'>
+                  <span
+                    onClick={() => visibilityHandler()}
+                    className='image-card__cover__btn'
+                  >
+                    view
+                  </span>
+                </div>
+              </div>
+            );
+          case 1:
+            return (
+              <div
+                ref={item}
+                key={data}
+                className={
+                  isVisible
+                    ? `image-card  col-span-2 row-span-5 col-start-5`
+                    : 'opacity-0'
+                }
+              >
+                {/* image */}
+                <div
+                  key={data}
+                  style={{
+                    backgroundImage: `url("${data}")`,
+                  }}
+                  className={`image-card__image`}
+                ></div>
+                {/* cover */}
+                <div className='image-card__cover'>
+                  <span
+                    onClick={() => visibilityHandler()}
+                    className='image-card__cover__btn'
+                  >
+                    view
+                  </span>
+                </div>
+              </div>
+            );
+          case 2:
+            return (
+              <div
+                ref={item}
+                key={data}
+                className={
+                  isVisible
+                    ? `image-card col-span-2 row-span-5 col-start-7 `
+                    : 'opacity-0'
+                }
+              >
+                {/* image */}
+                <div
+                  key={data}
+                  style={{
+                    backgroundImage: `url("${data}")`,
+                  }}
+                  className={`image-card__image`}
+                ></div>
+                {/* cover */}
+                <div className='image-card__cover'>
+                  <span
+                    onClick={() => visibilityHandler()}
+                    className='image-card__cover__btn'
+                  >
+                    view
+                  </span>
+                </div>
+              </div>
+            );
+          case 3:
+            return (
+              <div
+                ref={item}
+                key={data}
+                className={
+                  isVisible
+                    ? `image-card col-span-2 row-span-5 row-start-4 `
+                    : 'opacity-0'
+                }
+              >
+                {/* image */}
+                <div
+                  key={data}
+                  style={{
+                    backgroundImage: `url("${data}")`,
+                  }}
+                  className={`image-card__image`}
+                ></div>
+                {/* cover */}
+                <div
+                  onClick={() => visibilityHandler()}
+                  className='image-card__cover'
+                >
+                  <span className='image-card__cover__btn'>view</span>
+                </div>
+              </div>
+            );
+          case 4:
+            return (
+              <div
+                ref={item}
+                key={data}
+                className={
+                  isVisible
+                    ? `image-card col-span-2 row-span-5 col-start-3 row-start-4`
+                    : 'opacity-0'
+                }
+              >
+                {/* image */}
+                <div
+                  key={data}
+                  style={{
+                    backgroundImage: `url("${data}")`,
+                  }}
+                  className={`image-card__image`}
+                ></div>
+                {/* cover */}
+                <div
+                  onClick={() => visibilityHandler()}
+                  className='image-card__cover'
+                >
+                  <span className='image-card__cover__btn'>view</span>
+                </div>
+              </div>
+            );
+          default:
+            return (
+              <div
+                ref={item}
+                key={data}
+                className={
+                  isVisible
+                    ? `image-card col-span-4 row-span-3 col-start-5 row-start-6`
+                    : 'opacity-0'
+                }
+              >
+                {/* image */}
+                <div
+                  key={data}
+                  style={{
+                    backgroundImage: `url("${data}")`,
+                  }}
+                  className={`image-card__image`}
+                ></div>
+                {/* cover */}
+                <div
+                  onClick={() => visibilityHandler()}
+                  className='image-card__cover'
+                >
+                  <span className='image-card__cover__btn'>view</span>
+                </div>
+              </div>
+            );
+        }
+      default:
+        break;
+    }
+  };
+
+  return <Fragment>{pickItemType()}</Fragment>;
+};
+
+export default ItemGrid;
